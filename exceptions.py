2a40ac05e6b5f25ebfaddf3f992e7f92eb8959e7
yoni-j <yoni-j@drorisrael.org.il>
2020-10-18 23:52:52 +0300

    exceptions lesson

---
+++ exceptions.py
@@ -0,0 +1,25 @@
+#SyntaxError
+a = ,
+
+#IndexError
+L1=[1,2,3]
+print (L1[3])
+
+#ModuleNotFoundError
+import notamodule
+
+#ImportError
+from math import cube
+
+#KeyError
+D1={'1':"aa", '2':"bb", '3':"cc"}
+print(D1['4'])
+
+#TypeError
+print('2'+2)
+
+#ValueError
+print(int('xyz'))
+
+#NameError
+print(X)
